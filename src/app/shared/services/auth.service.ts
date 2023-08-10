f551b58c81dc65ce2ddfe75e55452fa26986e43b
mostafa-rayhan <mostafarayhan57@gmail.com>
2023-08-10 21:56:05 +0600

    services

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,43 @@
+
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { UserRole } from '../role.enum';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string): Observable<User> {
+    return this.http.post<User>('/api/auth/login', { username, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+        return user;
+      }));
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  signUp(username: string, password: string, role: UserRole): Observable<any> {
+    return this.http.post<any>('/api/auth/signup', { username, password, role });
+  }
+}
